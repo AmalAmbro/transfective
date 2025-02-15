0232d25984ade858d1897cad37acd9504b4f22d1
Amal Ambrose <amalambrose555@gmail.com>
2025-02-15 08:02:25 +0530

    other files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './assets/styles/main.css';
+import Header from './components/Header';
+import Spotlight from './components/Spotlight';
+import InnovativeSection from './components/InnovativeSection';
+import WhyChooseSection from './components/whychoose/WhyChooseSection';
+import Footer from './components/Footer';
+import TranslationSection from './components/TranslationSection';
+import FaqSection from './components/FaqSection';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Spotlight />
+      <InnovativeSection />
+      <TranslationSection />
+      <FaqSection />
+      <WhyChooseSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
